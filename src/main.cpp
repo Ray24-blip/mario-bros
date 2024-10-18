efa2c4a720c4bc467f6762f30eaab6ddd818dc97
Ray24-blip <A23310309@live.ceti.mx>
2024-10-18 23:33:17 +0000

    clase 3

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,18 @@
 #include<iostream>
 #include<Porcentaje.hpp>
+#include<list>
 
 int main(){
+    std::list<Porcentaje>focos;
     Porcentaje foco1,foco2,foco3;
   
+    focos. emplace_back(foco1);
+    focos. emplace_back(foco2);
+    focos. emplace_back(foco3);
+    for (auto &&f : focos)
+    {
+        f.Incrementar(10);
+    }
     
     foco1.Incrementar(10);
     foco1.Incrementar(10);
