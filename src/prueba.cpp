a0650db5d88a37ca222180015f7155c953960961
Ray24-blip <A23310309@live.ceti.mx>
2024-11-02 17:25:19 +0000

    EXAMEN

--- src/prueba.cpp
+++ src/prueba.cpp
@@ -12,25 +12,41 @@ int main(int argc, char const *argv[])
 {
     std::string reset_position;
     int frame = 0;
-    int a;
-    a=20;
-    auto can = Canvas(200,200);
-    can.DrawBlockCircle(10,10,10);
-    can.DrawBlockCircle(20,20,10);
-    can.DrawBlockCircle(30,10,10);
-    can.DrawBlockCircle(40,20,10);
-    can.DrawBlockCircle(50,10,10);
 
-    while (true)
+    while (true && frame < 100)
     {
+        auto can = Canvas(200, 200);
+        can.DrawBlockCircle(0 + frame, 10, 10, Color::Blue1);
+        can.DrawBlockCircle(10 + frame, 20, 10, Color::Yellow1);
+        can.DrawBlockCircle(20 + frame, 10, 10, Color::Black);
+        can.DrawBlockCircle(30 + frame, 20, 10, Color::Green1);
+        can.DrawBlockCircle(40 + frame, 10, 10, Color::Red1);
         Screen pantalla = Screen::Create(Dimension::Full(), Dimension::Full());
         Element lienzo = bgcolor(Color::BlueLight, border(vbox(canvas(&can))));
         Render(pantalla, lienzo);
         std::cout << reset_position;
         pantalla.Print();
         reset_position = pantalla.ResetPosition(true);
-        this_thread::sleep_for(0.1s);
+        this_thread::sleep_for(0.09s);
         frame++;
+    }
+
+    while (true && frame > 0)
+    {
+        auto can = Canvas(200, 200);
+        can.DrawBlockCircle(0 +  frame, 10, 10, Color::Blue1);
+        can.DrawBlockCircle(10 + frame, 20, 10, Color::Yellow1);
+        can.DrawBlockCircle(20 + frame, 10, 10, Color::Black);
+        can.DrawBlockCircle(30 + frame, 20, 10, Color::Green1);
+        can.DrawBlockCircle(40 + frame, 10, 10, Color::Red1);
+        Screen pantalla = Screen::Create(Dimension::Full(), Dimension::Full());
+        Element lienzo = bgcolor(Color::BlueLight, border(vbox(canvas(&can))));
+        Render(pantalla, lienzo);
+        std::cout << reset_position;
+        pantalla.Print();
+        reset_position = pantalla.ResetPosition(true);
+        this_thread::sleep_for(0.09s);
+        frame--;
     }
 
     return 0;
